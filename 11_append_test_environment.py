d12165f3022ace10c19e64f9fd1136f120bd1cbe
jamiespicer-demonbaby <136043579+jamiespicer-demonbaby@users.noreply.github.com>
2023-09-12 16:38:42 +1200

    Add files via upload

---
+++ 11_append_test_environment.py
@@ -0,0 +1,23 @@
+string_list = []
+string_list2 = []
+pizza_list = []
+
+words = "hi"
+worded = "there"
+worden = "jamie"
+phrase = "no"
+phrased = "yes"
+phrasing = "maybe"
+
+string_list.append(words)
+string_list.append(worded)
+string_list.append(worden)
+
+string_list2.append(phrase)
+string_list2.append(phrased)
+string_list2.append(phrasing)
+
+pizza_list.append(string_list)
+pizza_list.append(string_list2)
+
+print(pizza_list)
